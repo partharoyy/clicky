9afb000c6b287065dd1b3c65982b40cc70d7528b
partharoyy <parth@Parthas-MacBook-Pro.local>
2021-04-23 12:11:00 +0530

    v1.0

--- src/Components/UploadPics.js
+++ src/Components/UploadPics.js
@@ -3,36 +3,36 @@ import storage from '../firebase/config'
 
 const UploadPics = () => {
 
-    const [image, setImage] = useState(null)
     const [progress, setProgress] = useState(0)
     const [error, setError] = useState('')
     const [url, setUrl] = useState('')
 
     const onChangeHandler = (e) => {
-        if (e.target.files[0]) {
-            setImage(e.target.files[0])
+
+        const file = e.target.files[0]
+
+        if (file) {
+            const upload = storage.ref(`images/${file.name}`).put(file)
+            upload.on("state_changed",
+                snap => {
+                    const progress = (Math.round((snap.bytesTransferred / snap.totalBytes) * 100))
+                    setProgress(progress)
+                },
+                err => {
+                    setError(err)
+                },
+                () => {
+                    storage.ref('images')
+                        .child(file.name)
+                        .getDownloadURL()
+                        .then(url => {
+                            setUrl(url)
+                        })
+                })
         }
-    }
 
-    const onClickHandler = () => {
 
-        const upload = storage.ref(`images/${image.name}`).put(image)
-        upload.on("state_changed",
-            snap => {
-                const progress = (Math.round((snap.bytesTransferred / snap.totalBytes) * 100))
-                setProgress(progress)
-            },
-            err => {
-                setError(err)
-            },
-            () => {
-                storage.ref('images')
-                    .child(image.name)
-                    .getDownloadURL()
-                    .then(url => {
-                        setUrl(url)
-                    })
-            })
+
     }
 
     return (
@@ -43,7 +43,7 @@ const UploadPics = () => {
             <div className="input-div">
                 <h2>Add yours</h2>
                 <input type="file" id="upload" onChange={onChangeHandler} />
-                <label htmlFor="upload" className="label-btn" onClick={onClickHandler}>+</label>
+                <label htmlFor="upload" className="label-btn">+</label>
             </div>
         </div>
     )
