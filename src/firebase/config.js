fe566d45884ef9ba0bcbedb6f591b0b460b3b1f8
partharoyy <partharoyy88@gmail.com>
2021-07-29 17:30:41 +0530

    disabled firebase key file for now

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,24 +1,23 @@
-import firebase from 'firebase/app'
-import 'firebase/storage'
-import 'firebase/firestore'
-
+import firebase from "firebase/app";
+import "firebase/storage";
+import "firebase/firestore";
 
 // Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyBxkWoRdEDCx-h0u8W7HZv9yAMKlZmIhUU",
-    authDomain: "clicky-3c4dc.firebaseapp.com",
-    projectId: "clicky-3c4dc",
-    storageBucket: "clicky-3c4dc.appspot.com",
-    messagingSenderId: "520214197255",
-    appId: "1:520214197255:web:87c7416f19361dff84e6a2"
-};
+// var firebaseConfig = {
+//     apiKey: "AIzaSyBxkWoRdEDCx-h0u8W7HZv9yAMKlZmIhUU",
+//     authDomain: "clicky-3c4dc.firebaseapp.com",
+//     projectId: "clicky-3c4dc",
+//     storageBucket: "clicky-3c4dc.appspot.com",
+//     messagingSenderId: "520214197255",
+//     appId: "1:520214197255:web:87c7416f19361dff84e6a2"
+// };
 // Initialize Firebase
 if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseConfig);
 }
 
 const storage = firebase.storage();
 const db = firebase.firestore();
 const timestamp = firebase.firestore.FieldValue.serverTimestamp();
 
-export { storage, db, timestamp }
+export { storage, db, timestamp };
